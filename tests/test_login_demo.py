8859ca92ea3947e8fe5a36555b75bd6a522bbc11
Saurabh301085 <saurabhk301085@outlook.com>
2026-05-16 21:52:35 +0530

    Update test_login_demo.py

--- tests/test_login_demo.py
+++ tests/test_login_demo.py
@@ -10,16 +10,23 @@ class Test_login_dtls:
 
     def test_sauce(self, driver):
         sauce = test_login_page1(driver)
-        sauce.enter_username("standard_user")
+        sauce.enter_username("standard_user1")
         sauce.enter_password("secret_sauce")
         sauce.login()
-
         assert sauce.is_product_page_loaded()
+
+        sauce.enter_username("wrong_user")
+        sauce.enter_password("wrong_pass")
+        sauce.login()
+
+        error= sauce.error_msg()
+        assert "Username and password do not match" in error
+
         time.sleep(4)
 
-        product = test_add_to_cart(driver)
-        # options.add_argument("--disable-save-password-bubble")
-        # options.add_argument("--disable-extensions")
-        product.open_burger_menu()
-        product.close_burger_menu()
-        product.add_to_cart_item()
+        # product = test_add_to_cart(driver)
+        # # options.add_argument("--disable-save-password-bubble")
+        # # options.add_argument("--disable-extensions")
+        # product.open_burger_menu()
+        # product.close_burger_menu()
+        # product.add_to_cart_item()
